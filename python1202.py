458d6c54a62ae1a25ce7b1e28efa60810a0f0580
patipan <patipan.patisampita@gmail.com>
2024-08-05 15:11:43 +0700

    update

---
+++ python1202.py
@@ -0,0 +1,23 @@
+#2.Assignment Operator
+a = 10
+
+#2.1 Assing value
+b = a
+print("Assing value:",b)
+
+#2.2 Add and assign value
+b += a #=>: b = b + a
+print("Assing value:",b) #=>:20
+
+#2.3 Subtract and assign value
+b -= a #=>: b = b - a
+print("Assing value:",b) #=>:10
+
+#2.4 Multiply and assign value
+b *= a #=>: b = b * a
+print("Assing value:",b) #=>:100
+
+#2.5 bitwise lishift operator
+b <<= a #=>: b = b << a
+print("Assing value:",b) #=>:?
+
---
+++ python1203.py
@@ -0,0 +1,39 @@
+#3.Comparison Operator
+a = 10
+b = 12
+c = 5
+d = 2
+
+#3.1 Output=>: a > b is Fasle
+print('a > b is',a > b)
+
+#3.2 Output=>: a < b is True
+print('a < b is',a < b)
+
+#3.3 Output=>: a == b is False
+print('a == b is',a == b)
+
+#3.4 Output=>: a != b is  True
+print('a != b is',a != b)
+
+#3.5 Output=>: a >= b is  False
+print('a >= b is',a >= b)
+
+#3.6 Output=>: a <= b is  True
+print('a <= b is',a <= b)
+
+#3.7 equal to opearor
+print('c == d is',c == d)
+
+#3.8 not equal to opearor
+print('c != d is',c != d)
+
+#3.9 greater than operator
+print('c > d is',c > d)
+
+#3.10 less than operator
+print('c < d is',c < d)
+
+#3.11 greater than or equal to operator
+print('c >= d is',c >= d)
+
